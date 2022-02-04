179dfe08561d9329a16d3e9a11aa25678927bf17
negishiyukana <yukana.negishi@rakus-partners.co.jp>
2022-02-04 16:11:28 +0900

    exam04 form更新

--- src/main/java/com/example/form/UserForm.java
+++ src/main/java/com/example/form/UserForm.java
@@ -2,6 +2,35 @@ package com.example.form;
 
 public class UserForm {
 	
+	private String name;
+	private Integer age;
+	private String commnet;
 	
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public String getCommnet() {
+		return commnet;
+	}
+	public void setCommnet(String commnet) {
+		this.commnet = commnet;
+	}
+	@Override
+	public String toString() {
+		return "UserForm [name=" + name + ", age=" + age + ", commnet=" + commnet + "]";
+	}
 
+	
+	
 }
