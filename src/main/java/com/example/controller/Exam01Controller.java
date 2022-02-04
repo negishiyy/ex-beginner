4892e5b84e3e827ae1f9a7208f9d669c8c29a454
negishiyukana <yukana.negishi@rakus-partners.co.jp>
2022-02-04 12:26:13 +0900

    comment

--- src/main/java/com/example/controller/Exam01Controller.java
+++ src/main/java/com/example/controller/Exam01Controller.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/ex01")
+@RequestMapping("/exam-01")
 public class Exam01Controller {
 
 	@RequestMapping("")
