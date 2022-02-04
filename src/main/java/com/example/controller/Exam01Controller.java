3ee1a6ddc2d37ded0ecf074e40750ddad2019c0c
negishiyukana <yukana.negishi@rakus-partners.co.jp>
2022-02-04 11:56:28 +0900

    comment

---
+++ src/main/java/com/example/controller/Exam01Controller.java
@@ -0,0 +1,20 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ex01")
+public class Exam01Controller {
+
+	@RequestMapping("")
+	public String index() {
+		return "exam01";
+	}
+	
+	@RequestMapping("/result")
+		public String result() {
+			return "exam01-result";
+		}
+	}
+
